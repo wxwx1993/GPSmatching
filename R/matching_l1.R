b50fbf05cc8cc2a0253eadc45cf2445c263b8dd3
wxwx1993 <wuxiao199301@126.com>
2020-01-14 18:24:43 -0500

    Updated matching function

--- R/matching_l1.R
+++ R/matching_l1.R
@@ -1,13 +1,23 @@
-# Matching function using L1 distance on single exposure level w
+#' Matching function using L1 distance on single exposure level w
+#'
+#' @param dataset a completed observational data frame or matrix containing (Y, w, c).
+#' @param e_gps_pred a vector of predicted gps values obtained by Machine learning methods.
+#' @param e_gps_std_pred a vector of predicted std of gps obtained by Machine learning methods.
+#' @param w the targeted single exposure levels.
+#' @param w_resid the standardized residuals for w.
+#' @param scale a specified scale parameter to control the relative weight that is attributed to the distance measures of the exposure versus the GPS estimates (Default is 0.5).
+#' @param delta_n a specified caliper parameter on the exposure (Default is 1).
+#' @return
+#' \code{dp}: The function returns a data.table saved the matched points on by single exposure level w by the proposed GPS matching approaches.
 #' @export
 
 matching_l1 <- function(dataset,
                         e_gps_pred,
                         e_gps_std_pred,
                         w,
-                        delta_n=1,
                         w_resid,
-                        scale)
+                        delta_n=1,
+                        scale=0.5)
 {
   w_new <- (w-e_gps_pred)/e_gps_std_pred
   p.w <- approx(density(w_resid,na.rm = TRUE)$x,density(w_resid,na.rm = TRUE)$y,xout=w_new,rule=2)$y
