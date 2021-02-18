52639b21ac3797ec5ab4a67b2791b1389a9fc2a7
naeemkh <khoshnevis.naeem@gmail.com>
2021-02-17 21:26:00 -0600

    added new tests.

--- tests/testthat/test-GPSmatching_smooth.R
+++ tests/testthat/test-GPSmatching_smooth.R
@@ -1,3 +1,15 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("gpsmatching_smooth tests.", {
+  
+   val1 <- generate_kernel(seq(-1.1,1,0.01))
+   
+   expect_equal(val1[10], 0.2395511, tolerance=0.0001)
+   expect_equal(val1[100], 0.396536, tolerance=0.0001)
+   expect_equal(val1[150], 0.3697277, tolerance=0.0001)
+
+   sq <- seq(0.2,2,0.2)
+   val2 <- w_fun(0.25, pseudo_pop_covar_test$w, sq)
+
+   expect_equal(length(sq),length(val2))
+   expect_equal(val2[2],0.01054791, tolerance=0.0001)   
+
 })
