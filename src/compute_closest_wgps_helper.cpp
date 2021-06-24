2863285edd885def4f99d2254628c932056406d5
naeemkh <khoshnevis.naeem@gmail.com>
2021-06-24 13:03:10 -0500

    polish OpenMP

--- src/compute_closest_wgps_helper.cpp
+++ src/compute_closest_wgps_helper.cpp
@@ -26,9 +26,12 @@ IntegerVector compute_closest_wgps_helper(NumericVector a,
   NumericVector tmp(size_a);
   IntegerVector out(size_b);
 
+
+  #if defined(_OPENMP)
   int nthread = omp_get_max_threads();
   omp_set_num_threads(nthread);
   #pragma omp parallel for
+  #endif
   for(int i = 0; i < size_b; ++i) {
     for(int j=0; j < size_a; ++j) {
 
