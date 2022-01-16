59ff649ed81d436b2f72ff09a0dad9643d97ad41
naeemkh <khoshnevis.naeem@gmail.com>
2022-01-16 12:13:01 -0500

    add test for check parameters.

--- tests/testthat/test-generate_pseudo_pop.R
+++ tests/testthat/test-generate_pseudo_pop.R
@@ -105,6 +105,173 @@ test_that("generate_pseudo_pop works as expected.", {
                                    scale = 0.5,
                                    nthread = 1))
 
+  # expect error with wrong gps_model
+  expect_error(generate_pseudo_pop(mydata$Y,
+                                   mydata$treat,
+                                   mydata[c("cf1","cf2","cf3","cf4","cf5",
+                                            "cf6","year","region")],
+                                   ci_appr = "matching",
+                                   pred_model = "fl",
+                                   gps_model = "half-parametric",
+                                   trim_quantiles = c(0.04,0.96),
+                                   optimized_compile = TRUE,
+                                   sl_lib = c("m_xgboost"),
+                                   covar_bl_method = "absolute",
+                                   covar_bl_trs = 0.1,
+                                   covar_bl_trs_type = "mean",
+                                   max_attempt = 1,
+                                   matching_fun = "matching_l1",
+                                   delta_n = 1,
+                                   scale = 0.5,
+                                   nthread = 1))
+
+  # expect error with missing gps parameter
+  expect_error(generate_pseudo_pop(mydata$Y,
+                                   mydata$treat,
+                                   mydata[c("cf1","cf2","cf3","cf4","cf5",
+                                            "cf6","year","region")],
+                                   ci_appr = "matching",
+                                   pred_model = "sl",
+                                   gps_model = "parametric",
+                                   trim_quantiles = c(0.04,0.96),
+                                   optimized_compile = TRUE,
+                                   #sl_lib = c("m_xgboost"),
+                                   covar_bl_method = "absolute",
+                                   covar_bl_trs = 0.1,
+                                   covar_bl_trs_type = "mean",
+                                   max_attempt = 1,
+                                   matching_fun = "matching_l1",
+                                   delta_n = 1,
+                                   scale = 0.5,
+                                   nthread = 1))
+
+
+  # expect error with wrong max attempt
+  expect_error(generate_pseudo_pop(mydata$Y,
+                                   mydata$treat,
+                                   mydata[c("cf1","cf2","cf3","cf4","cf5",
+                                            "cf6","year","region")],
+                                   ci_appr = "matching",
+                                   pred_model = "sl",
+                                   gps_model = "parametric",
+                                   trim_quantiles = c(0.04,0.96),
+                                   optimized_compile = TRUE,
+                                   sl_lib = c("m_xgboost"),
+                                   covar_bl_method = "absolute",
+                                   covar_bl_trs = 0.1,
+                                   covar_bl_trs_type = "mean",
+                                   max_attempt = "five",
+                                   matching_fun = "matching_l1",
+                                   delta_n = 1,
+                                   scale = 0.5,
+                                   nthread = 1))
+
+  # expect error with wrong optimized compile answer
+  expect_error(generate_pseudo_pop(mydata$Y,
+                                   mydata$treat,
+                                   mydata[c("cf1","cf2","cf3","cf4","cf5",
+                                            "cf6","year","region")],
+                                   ci_appr = "matching",
+                                   pred_model = "sl",
+                                   gps_model = "parametric",
+                                   trim_quantiles = c(0.04,0.96),
+                                   optimized_compile = "YES",
+                                   sl_lib = c("m_xgboost"),
+                                   covar_bl_method = "absolute",
+                                   covar_bl_trs = 0.1,
+                                   covar_bl_trs_type = "mean",
+                                   max_attempt = 1,
+                                   matching_fun = "matching_l1",
+                                   delta_n = 1,
+                                   scale = 0.5,
+                                   nthread = 1))
+
+  # expect error with wrong covar_bl_method
+  expect_error(generate_pseudo_pop(mydata$Y,
+                                   mydata$treat,
+                                   mydata[c("cf1","cf2","cf3","cf4","cf5",
+                                            "cf6","year","region")],
+                                   ci_appr = "matching",
+                                   pred_model = "sl",
+                                   gps_model = "parametric",
+                                   trim_quantiles = c(0.04,0.96),
+                                   optimized_compile = TRUE,
+                                   sl_lib = c("m_xgboost"),
+                                   covar_bl_method = "nonabsolute",
+                                   covar_bl_trs = 0.1,
+                                   covar_bl_trs_type = "mean",
+                                   max_attempt = 1,
+                                   matching_fun = "matching_l1",
+                                   delta_n = 1,
+                                   scale = 0.5,
+                                   nthread = 1))
+
+
+  # expect error with wrong scale
+  expect_error(generate_pseudo_pop(mydata$Y,
+                                   mydata$treat,
+                                   mydata[c("cf1","cf2","cf3","cf4","cf5",
+                                            "cf6","year","region")],
+                                   ci_appr = "matching",
+                                   pred_model = "sl",
+                                   gps_model = "parametric",
+                                   trim_quantiles = c(0.04,0.96),
+                                   optimized_compile = TRUE,
+                                   sl_lib = c("m_xgboost"),
+                                   covar_bl_method = "absolute",
+                                   covar_bl_trs = 0.1,
+                                   covar_bl_trs_type = "mean",
+                                   max_attempt = 1,
+                                   matching_fun = "matching_l1",
+                                   delta_n = 1,
+                                   scale = 1.5,
+                                   nthread = 1))
+
+  #expect error with wrong answer in using cove transform.
+  expect_error(generate_pseudo_pop(mydata$Y,
+                                   mydata$treat,
+                                   mydata[c("cf1","cf2","cf3","cf4","cf5",
+                                            "cf6","year","region")],
+                                   ci_appr = "matching",
+                                   pred_model = "sl",
+                                   gps_model = "parametric",
+                                   trim_quantiles = c(0.04,0.96),
+                                   optimized_compile = TRUE,
+                                   use_cov_transform = "YES",
+                                   transformers = list("pow2","pow3"),
+                                   sl_lib = c("m_xgboost"),
+                                   covar_bl_method = "absolute",
+                                   covar_bl_trs = 0.1,
+                                   covar_bl_trs_type = "mean",
+                                   max_attempt = 4,
+                                   matching_fun = "matching_l1",
+                                   delta_n = 1,
+                                   scale = 0.5,
+                                   nthread = 1))
+
+
+  #expect error with wrong transformers.
+  expect_error(generate_pseudo_pop(mydata$Y,
+                                   mydata$treat,
+                                   mydata[c("cf1","cf2","cf3","cf4","cf5",
+                                            "cf6","year","region")],
+                                   ci_appr = "matching",
+                                   pred_model = "sl",
+                                   gps_model = "parametric",
+                                   trim_quantiles = c(0.04,0.96),
+                                   optimized_compile = TRUE,
+                                   use_cov_transform = TRUE,
+                                   transformers = numeric(),
+                                   sl_lib = c("m_xgboost"),
+                                   covar_bl_method = "absolute",
+                                   covar_bl_trs = 0.1,
+                                   covar_bl_trs_type = "mean",
+                                   max_attempt = 4,
+                                   matching_fun = "matching_l1",
+                                   delta_n = 1,
+                                   scale = 0.5,
+                                   nthread = 1))
+
 
   # expect error with missing parameter
   expect_error(generate_pseudo_pop(mydata$Y,
@@ -179,6 +346,34 @@ test_that("generate_pseudo_pop works as expected.", {
   expect_equal(nrow(ps_pop4$pseudo_pop), 460)
   expect_equal(ps_pop4$adjusted_corr_results$mean_absolute_corr,
                0.0795775617737965,
+               tolerance = 0.000001)
+
+
+  expect_warning(ps_pop5 <- generate_pseudo_pop(mydata$Y,
+                                 mydata$treat,
+                                 mydata[c("cf1","cf2","cf4")],
+                                 ci_appr = "matching",
+                                 pred_model = "sl",
+                                 gps_model = "parametric",
+                                 trim_quantiles = c(0.04,0.96),
+                                 optimized_compile = TRUE,
+                                 use_cov_transform = TRUE,
+                                 transformers = list("pow2","pow3"),
+                                 sl_lib = c("m_xgboost"),
+                                 covar_bl_method = "absolute",
+                                 covar_bl_trs = 0.02,
+                                 covar_bl_trs_type = "mean",
+                                 max_attempt = 7,
+                                 matching_fun = "matching_l1",
+                                 delta_n = 1,
+                                 scale = 0.5,
+                                 nthread = 1))
+
+  expect_equal(class(ps_pop5),"gpsm_pspop")
+  expect_false(ps_pop5$passed_covar_test)
+  expect_equal(nrow(ps_pop4$pseudo_pop), 460)
+  expect_equal(ps_pop5$adjusted_corr_results$mean_absolute_corr,
+               0.05900387,
                tolerance = 0.000001)
 
 
