4e2e6e5d6ac853446ddf19ea00082efd0a7ab636
naeemkh <khoshnevis.naeem@gmail.com>
2022-01-16 00:42:43 -0500

    improve outcome tests.

--- tests/testthat/test-estimate_semipmetric_erf.R
+++ tests/testthat/test-estimate_semipmetric_erf.R
@@ -1,5 +1,6 @@
 test_that("estimate-semi-erf works as expected ", {
 
+  # Weighting
   set.seed(245)
   gam_model <-  estimate_semipmetric_erf(Y ~ s(w) + cf5,
                                   family = gaussian,
@@ -7,5 +8,24 @@ test_that("estimate-semi-erf works as expected ", {
                                   ci_appr = "weighting")
   coef_val <- gam_model$coefficients[2]
   expect_equal(coef_val[[1]], 5.484447, tolerance = 0.00001)
+
+
+  # Matching
+  set.seed(897)
+  gam_model <-  estimate_semipmetric_erf(Y ~ s(w) + cf5,
+                                         family = gaussian,
+                                         data = pseudo_pop_weight_test,
+                                         ci_appr = "matching")
+  coef_val <- gam_model$coefficients[2]
+  expect_equal(coef_val[[1]], 7.350412, tolerance = 0.00001)
+
+
+
+  # Wrong ci_appr.
+  expect_error(estimate_semipmetric_erf(Y ~ s(w) + cf5,
+                                        family = gaussian,
+                                        data = pseudo_pop_weight_test,
+                                        ci_appr = "grounding"))
+
 
 })
