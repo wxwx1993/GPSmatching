4d58642e401927179f599cf1318143250a5561be
naeemkh <khoshnevis.naeem@gmail.com>
2022-03-07 22:09:54 -0500

    drop extra parallelism.

--- R/create_matching.R
+++ R/create_matching.R
@@ -62,41 +62,55 @@ create_matching <- function(dataset, bin_seq = NULL, gps_model = "parametric",
 
     lfp <- get_options("logger_file_path")
 
-    p_c_t_s <- proc.time()
-    cl <- parallel::makeCluster(nthread, type="PSOCK",
-                                outfile= lfp)
-
+    # p5_c_t_s <- proc.time()
+    # cl <- parallel::makeCluster(nthread, type="PSOCK",
+    #                             outfile= lfp)
 
     # seed_val <- .Random.seed
     # parallel::clusterSetRNGStream(cl = cl, iseed = seed_val)
 
-    parallel::clusterEvalQ(cl, {library("CausalGPS")})
-    p_c_t_e <- proc.time()
+    # parallel::clusterEvalQ(cl, {library("CausalGPS")})
+    # p_c_t_e <- proc.time()
 
-    logger::log_debug("Wall clock time to create cluster with ",
-                      "{nthread} core(s): {p_c_t_e[[3]] - p_c_t_s[[3]]} s.")
-    parallel::clusterExport(cl=cl,
-                          varlist = c("bin_num", "matching_fun", "dataset",
-                                      "gps_mx", "w_mx", "delta_n", "scale",
-                                      "nthread", "compute_closest_wgps",
-                                      "compute_resid", "compute_density"),
-                          envir=environment())
+    # logger::log_debug("Wall clock time to create cluster with ",
+    #                   "{nthread} core(s): {p_c_t_e[[3]] - p_c_t_s[[3]]} s.")
+    # parallel::clusterExport(cl=cl,
+    #                       varlist = c("bin_num", "matching_fun", "dataset",
+    #                                   "gps_mx", "w_mx", "delta_n", "scale",
+    #                                   "nthread", "compute_closest_wgps",
+    #                                   "compute_resid", "compute_density"),
+    #                       envir=environment())
 
-    matched_set <-  parallel::parLapply(cl,
-                                        bin_num,
-                                        matching_fun,
-                                        dataset=dataset[[1]],
-                                        e_gps_pred = dataset[[2]],
-                                        e_gps_std_pred = dataset[[3]],
-                                        w_resid=dataset[[4]],
-                                        gps_mx = gps_mx,
-                                        w_mx = w_mx,
-                                        gps_model = gps_model,
-                                        delta_n = delta_n,
-                                        scale = scale,
-                                        nthread = nthread,
-                                        optimized_compile = optimized_compile)
-    parallel::stopCluster(cl)
+    # matched_set <-  parallel::parLapply(cl,
+    #                                     bin_num,
+    #                                     matching_fun,
+    #                                     dataset=dataset[[1]],
+    #                                     e_gps_pred = dataset[[2]],
+    #                                     e_gps_std_pred = dataset[[3]],
+    #                                     w_resid=dataset[[4]],
+    #                                     gps_mx = gps_mx,
+    #                                     w_mx = w_mx,
+    #                                     gps_model = gps_model,
+    #                                     delta_n = delta_n,
+    #                                     scale = scale,
+    #                                     nthread = nthread,
+    #                                     optimized_compile = optimized_compile)
+    # parallel::stopCluster(cl)
+
+    matched_set <-  lapply(bin_num,
+                           matching_fun,
+                           dataset=dataset[[1]],
+                           e_gps_pred = dataset[[2]],
+                           e_gps_std_pred = dataset[[3]],
+                           w_resid=dataset[[4]],
+                           gps_mx = gps_mx,
+                           w_mx = w_mx,
+                           gps_model = gps_model,
+                           delta_n = delta_n,
+                           scale = scale,
+                           nthread = nthread,
+                           optimized_compile = optimized_compile)
+    #parallel::stopCluster(cl)
 
   e_t_m <- proc.time()
   logger::log_debug("Finished generating matched set (Wall clock time:  ",
