5594da012720856e14caa268b116f72dcf4cc49f
Xiao Wu <xiao_wu@mail.harvard.edu>
2023-02-17 14:07:29 -0800

    Update causalgps_smooth.R

--- R/causalgps_smooth.R
+++ R/causalgps_smooth.R
@@ -31,11 +31,11 @@ generate_kernel <- function(t) {
 w_fun <- function(bw, matched_w, w_vals){
     w_avals <- NULL
     for (w_val in w_vals) {
-      w_std <- (matched_w - w_val) / bw
-      kern_std <- generate_kernel(w_std) / bw
-      tmp_mean <- mean(w_std ^ 2 * kern_std)
-      w_avals <- c(w_avals, tmp_mean * (generate_kernel(0) / bw) /
-                   (mean(kern_std) * tmp_mean - mean(w_std * kern_std) ^ 2))
+    w_std <- (matched_w - w_val) / bw
+    kern_std <- generate_kernel(w_std) / bw
+    tmp_mean <- weighted.mean(w_std ^ 2 * kern_std, w = matched_cw)
+    w_avals <- c(w_avals, tmp_mean * (generate_kernel(0) / bw) /
+                   (weighted.mean(kern_std, w = matched_cw) * tmp_mean - weighted.mean(w_std * kern_std, w = matched_cw) ^ 2))
   }
-  return(w_avals / length(matched_w))
+  return(w_avals / sum(matched_cw))
 }
