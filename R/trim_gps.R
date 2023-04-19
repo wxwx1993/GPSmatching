fd941649a8402c8cfb8d9e52bf32eee6bd5a4802
naeemkh <khoshnevis.naeem@gmail.com>
2023-04-19 12:59:37 -0400

    add trimming for gps values

--- R/trim_gps.R
+++ R/trim_gps.R
@@ -15,6 +15,31 @@
 #'
 trim_gps <- function(gps_obj, trim_quantiles){
 
-  gps_obj <- gps_obj
+  if ((trim_quantiles[1] < 0 || trim_quantiles[1] > 1) ||
+      (trim_quantiles[2] < 0 || trim_quantiles[2] > 1) ||
+      (trim_quantiles[1] > trim_quantiles[2])) {
+    stop(paste("trim_quntiles should be in the [0,1] range,",
+               " and the first quantile should be less than the second one."))
+  }
+
+  if (!inherits(gps_obj, "cgps_gps")){
+    stop("Provided gps_obj is not an standard gps object.")
+  }
+
+  df1 <- gps_obj$dataset
+
+  # get trim quantiles and trim data
+  q1 <- stats::quantile(df1[["gps"]], trim_quantiles[1])
+  q2 <- stats::quantile(df1[["gps"]], trim_quantiles[2])
+
+  logger::log_debug("{trim_quantiles[1]*100}% quantile for trim: {q1}")
+  logger::log_debug("{trim_quantiles[2]*100}% for trim: {q2}")
+  df1 <- df1[df1[["gps"]] <= q2  & df1[["gps"]] >= q1, ]
+
+  gps_obj$dataset <- df1
+
+  #TODO: Double-check if we need to also modify the min max values.
+
+  return(gps_obj)
 
 }
--- functional_tests/ft_estimate_gps.R
+++ functional_tests/ft_estimate_gps.R
@@ -8,6 +8,7 @@ s_data <- generate_syn_data(sample_size=200,
 data_with_gps_test <- estimate_gps(s_data[, c("id", "w")],
                                    s_data[, c("id", "cf1", "cf2", "cf3",
                                               "cf4","cf5", "cf6")],
-                                   internal_use = TRUE,
                                    sl_lib = c("SL.xgboost","SL.earth",
                                               "SL.gam","SL.ranger"))
+
+head(data_with_gps_test$dataset)
