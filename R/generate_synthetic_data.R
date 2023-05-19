61816aa77ce1cb35d2fa36ac164ebb976133fa99
naeemkh <khoshnevis.naeem@gmail.com>
2023-05-19 13:44:40 -0400

    refactor treatment

--- R/generate_synthetic_data.R
+++ R/generate_synthetic_data.R
@@ -77,15 +77,6 @@ generate_syn_data <- function(sample_size = 1000,
     stop(paste("cova_spec: ", cova_spec, ", is not a valid value."))
   }
 
-  calculate_treatment <- function(cf, cf5, cf6) {
-    return(-0.8 + 0.1 * cf[,1]
-                + 0.1 * cf[,2]
-                - 0.1 * cf[,3]
-                + 0.2 * cf[,4]
-                + 0.1 * cf5
-                + 0.1 * cf6)
-  }
-
   size <- sample_size
 
   #pre-treatment variables (confounders)
@@ -97,42 +88,45 @@ generate_syn_data <- function(sample_size = 1000,
   cf5 <- sample(c((-2):2), size, replace = TRUE)
   cf6 <- stats::runif(size, min=-3, max=3)
 
+  tmp_treat <- (- 0.8 + 0.1 * cf[, 1]
+                      + 0.1 * cf[, 2]
+                      - 0.1 * cf[, 3]
+                      + 0.2 * cf[, 4]
+                      + 0.1 * cf5
+                      + 0.1 * cf6)
+
   if (gps_spec == 1) {
 
-    treat <- calculate_treatment(cf, cf5, cf6) * 9
-             + 17 + stats::rnorm(size, sd=5)
+    treat <- (tmp_treat * 9 + 17  + stats::rnorm(size,sd=5))
 
   } else if (gps_spec == 2) {
 
-    treat <- calculate_treatment(cf, cf5, cf6) * 15 + 22 + stats::rt(size,2)
+    treat <- (tmp_treat * 15 + 22 + stats::rt(size,2))
     treat[which(treat < (-5))] <- (-5)
     treat[which(treat > (25))] <- (25)
 
   } else if (gps_spec == 3) {
 
-    treat <- calculate_treatment(cf, cf5, cf6) * 9
-             + 1.5 * cf[ , 3] ^ 2 + stats::rnorm(size, mean = 0, sd = 5) + 15
+    treat <- (tmp_treat * 9 + 1.5 * cf[ , 3] ^ 2 +
+              stats::rnorm(size, mean = 0, 5) + 15)
 
   } else if (gps_spec == 4) {
 
-    treat <- (49 * exp(calculate_treatment(cf, cf5, cf6)) / (1 + exp(calculate_treatment(cf, cf5, cf6))) - 6 +
-                stats::rnorm(size, sd=5))
-
+    treat <- (49 * exp(tmp_treat)
+            / (1 + exp(tmp_treat)) - 6 + stats::rnorm(size, sd=5))
   } else if (gps_spec == 5) {
 
-    treat <- (42 / (1 + exp(calculate_treatment(cf, cf5, cf6))) - 18 +
-                stats::rnorm(size,sd=5))
+    treat <- (42 / (1 + exp(tmp_treat)) - 18 + stats::rnorm(size,sd=5))
 
   } else if (gps_spec == 6) {
 
-    treat <- (log(abs(calculate_treatment(cf, cf5, cf6))) * 7 + 13 +
-                stats::rnorm(size,sd=4))
+    treat <- (log(abs(tmp_treat)) * 7 + 13 + stats::rnorm(size,sd=4))
 
   } else if (gps_spec == 7) {
 
-    treat <- calculate_treatment(cf, cf5, cf6) * 15 + 22 + stats::rt(size,2)
-
+    treat <- (tmp_treat * 15 + 22 + stats::rt(size,2))
   } else {
+
     stop(paste("gps_spec: ", gps_spec, ", is not a valid value."))
   }
 
@@ -148,7 +142,7 @@ generate_syn_data <- function(sample_size = 1000,
     # Calculate Y
     Y <- -((1 + cf_sum * 10 - 2 * cf5 - 2 * cf6 + (treat - 20) * cf_part
             - 0.13 ^ 2 * (treat - 20) ^ 2)) +
-         rand_norm
+      rand_norm
   } else {
     # TODO: This part is kept for backward compatibility.
     Y <- as.numeric()
@@ -156,16 +150,16 @@ generate_syn_data <- function(sample_size = 1000,
     # non-vectorized Y
     for (i in 1:size) {
       Y[i] <- ((-(1 + (sum(c(0.2, 0.2, 0.3, -0.1) * cf[i, ]))
-                    * 10 - 2 * cf5[i] - 2 * cf6[i] + (treat[i] - 20)
-                    * (0.1 + 0.1 * cf[i,4] + 0.1 * cf5[i]
-                    + 0.1 * cf[i,3] ^ 2 - 0.13 ^ 2 * (treat[i] - 20) ^ 2)))
-                    + stats::rnorm(1, mean = 0, sd = outcome_sd))
+                  * 10 - 2 * cf5[i] - 2 * cf6[i] + (treat[i] - 20)
+                  * (0.1 + 0.1 * cf[i,4] + 0.1 * cf5[i]
+                     + 0.1 * cf[i,3] ^ 2 - 0.13 ^ 2 * (treat[i] - 20) ^ 2)))
+               + stats::rnorm(1, mean = 0, sd = outcome_sd))
     }
   }
 
   if (cova_spec == 1) {
 
-    cf = cf
+    cf <- cf
 
   } else if (cova_spec == 2) {
 
